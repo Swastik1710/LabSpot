fe74edeff11414a0db7a6eb6146c906deea15d40
Swastik1710 <swastikrajsingh7777@gmail.com>
2020-09-26 19:45:19 +0530

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+////alert('Hello, Welcome to my 1st website');
+/*eslint-env browser*/
+//var listItems = document.getElementById("menu-list").getElementsByTagName("li");
+//
+//listItems.addEventListener("click", activate);
+//
+//function activate() {
+//    listItems.innerHTML.style.backgroundColor = "white";
+//    listItems.innerHTML.style.color = "black";
+//}
+
+//var preloader = document.getElementById('loading');
+//
+//function func() {
+//    preloader.style.display = "none";
+//}
