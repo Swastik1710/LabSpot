9dc4eec815551269c0e58a34bfce82049b9cb2d7
Swastik1710 <swastikrajsingh7777@gmail.com>
2021-08-03 16:30:28 +0530

    Added install button

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,3 @@
-// const divInstall = document.getElementById('installContainer');
-// const butInstall = document.getElementById('butInstall');
-
-// window.addEventListener('beforeinstallprompt', (event) => {
-//     console.log('👍', 'beforeinstallprompt', event);
-//     // Stash the event so it can be triggered later.
-//     window.deferredPrompt = event;
-//     // Remove the 'hidden' class from the install button container
-//     divInstall.classList.toggle('hidden', false);
-// });
-
-// butInstall.addEventListener('click', async () => {
-//     console.log('👍', 'butInstall-clicked');
-//     const promptEvent = window.deferredPrompt;
-//     if (!promptEvent) {
-//         // The deferred prompt isn't available.
-//         return;
-//     }
-//     // Show the install prompt.
-//     promptEvent.prompt();
-//     // Log the result
-//     const result = await promptEvent.userChoice;
-//     console.log('👍', 'userChoice', result);
-//     // Reset the deferred prompt variable, since
-//     // prompt() can only be called once.
-//     window.deferredPrompt = null;
-//     // Hide the install button.
-//     divInstall.classList.toggle('hidden', true);
-// });
-
-// window.addEventListener('appinstalled', (event) => {
-//     console.log('👍', 'appinstalled', event);
-//     // Clear the deferredPrompt so it can be garbage collected
-//     window.deferredPrompt = null;
-// });
-
 window.addEventListener('load', () => {
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('serviceWorker.js');
