f45103976214468648a7d7f15a6554b8761bad8c
Swastik1710 <swastikrajsingh7777@gmail.com>
2022-10-10 23:42:03 +0530

    Added Search functionality

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,7 @@ window.addEventListener('load', () => {
 //     document.querySelector("body").style.visibility = "visible";
 //   }
 // };
+
 var loader = document.getElementById("load");
 window.addEventListener ("load", function() {
   loader.style.display = 'none';
@@ -49,29 +50,33 @@ window.onbeforeunload = () => {
   }
 };
 
-// MODALS
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("Btn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function search_lab() {
+  let input = document.getElementById('searchbar').value
+  let list = document.getElementById('search')
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('tags')
+  if(input=="")
+  {
+    list.style.display="none";
   }
-} 
+  else
+  {
+    list.style.display="block";
+  }
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="block";                 
+      }
+  }
+}
+let searchbar = document.getElementById('searchbar')
+let list = document.getElementById('search')
+window.onclick = function(event) {
+  if (event.target!= list && event.target!=searchbar) {
+    list.style.display = "none";
+  }
+}
